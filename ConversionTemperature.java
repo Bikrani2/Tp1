8f77bf8fb9fc51f6f097339ec19b086ec3b962d5
Fatima Zahrae BIKRANI <bikrani.fatimazahrae@gmail.com>
2024-10-30 17:28:34 +0100

    Update ConversionTemperature.java

--- ConversionTemperature.java
+++ ConversionTemperature.java
@@ -1,8 +1,10 @@
 class ConversionTemperature{
+  
   public static void main(String[] args){
     double temperatureCelsius=Double.parseDouble(args[0]);
     double temperatureFahrenheit=(9.0/5)*temperatureCelsius+32;
     System.out.println("La temperature passé en argument en degres Celsius: "+temperatureCelsius);
-    System.out.println(temperatureCelsius+" en deres Celsius equivalent à "+temperatureFahrenheit+" degres Fahrenheit");
+    System.out.println(temperatureCelsius+" en degres Celsius equivalent à "+temperatureFahrenheit+" degres Fahrenheit");
   }
+  
 }
