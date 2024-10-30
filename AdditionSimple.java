eea6380c14f918e15f3e8e9b5b13a5b41ef77e28
Fatima Zahrae BIKRANI <bikrani.fatimazahrae@gmail.com>
2024-10-30 17:26:47 +0100

    Update AdditionSimple.java

--- AdditionSimple.java
+++ AdditionSimple.java
@@ -1,6 +1,8 @@
 public class AdditionSimple{
+  
   public static void main(String[] args){
     double somme=Double.parseDouble(args[0])+Double.parseDouble(args[1]);
     System.out.println("La somme des deux nombres est: "+somme);
   }
+  
 }
